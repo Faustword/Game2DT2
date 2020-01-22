886c97d9bb3dbbd27d2bd55c9005cbe0f5c45756
Faustword <59095995+Faustword@users.noreply.github.com>
2020-01-22 13:32:49 +0100

    Salto
    Añadida mecánica de Salto al jugador

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody2D rigidbody;
+
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
+
+
