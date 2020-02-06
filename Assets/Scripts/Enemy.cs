c1d9fa5e4a334013f3257dded7590d7bac047058
Faustword <59095995+Faustword@users.noreply.github.com>
2020-02-06 18:41:17 +0100

    Nuevo Enemigo
    -Creado un enemigo que se mueve entre dos puntos, sin poder salir de ellos.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    public float runningSpeed = 1.5f;
+    public static bool turnAround;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float currentRunningSpeed = runningSpeed;
+
+        if(turnAround == true)
+        {
+            currentRunningSpeed = runningSpeed;
+            transform.eulerAngles = new Vector3(0, 180.0f, 0);
+        }
+        else
+        {
+            currentRunningSpeed = -runningSpeed;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            rigidbody.velocity = new Vector2(currentRunningSpeed, rigidbody.velocity.y); 
+        }
+    }
+
+}
---
+++ Assets/Scripts/TriggerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMovement : MonoBehaviour
+{
+    public bool movingForward;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(movingForward == true)
+        {
+            Enemy.turnAround = true;
+        }
+        else
+        {
+            Enemy.turnAround = false;
+        }
+    }
+}
