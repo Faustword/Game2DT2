fb3cfd0246e8785e5e2507c7b9ad69092ecafc6b
Faustword <59095995+Faustword@users.noreply.github.com>
2020-02-19 18:55:50 +0100

    Fondo Parallax
    -Añadido un Fondo por Capas
    -Programado el movimiento del fondo para dar un efecto Parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float speed = 0.0f;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.rigidbody.velocity = new Vector2(speed, 0);
+
+        float parentPosition = this.transform.parent.transform.position.x;
+
+        if(this.transform.position.x - parentPosition >= 19.17f)
+        {
+            this.transform.position = new Vector3(parentPosition - 19.17f, this.transform.position.y, this.transform.position.z);
+        }
+    }
+}
